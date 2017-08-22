888765f8fe8468f35788325c1456b1e1683db693
Niloy <sudipta980@gmail.com>
2017-08-22 18:32:14 +0000

    Fix create and error msg

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,8 +6,20 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to article_show(@article)
+    #@article.save
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+    #redirect_to article_path(@article)
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+    
+    
   end
   
   private
